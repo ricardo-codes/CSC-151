ae859104c5142f9cdbd48aa3df3810948f228b19
Ricardo Kelly <rickpeace1234567@gmail.com>
2026-02-09 02:14:43 -0500

    Add M1T1 C++ program

---
+++ m1t1.cpp
@@ -0,0 +1,19 @@
+*
+   CSC 134
+   M1T1 - First C++ Program
+   Your Name: Ricardo
+   This program demonstrates basic C++ output
+*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // Display a simple message to the screen
+    cout << "Hello, world!" << endl;
+    cout << "My name is Ricardo." << endl;
+    cout << "This is my first C++ program for CSC 134." << endl;
+
+    return 0;
+}
