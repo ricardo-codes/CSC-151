b722339d258272e3f874e200150cd703129fc6fa
Ricardo Kelly <rickpeace1234567@gmail.com>
2026-03-09 02:25:19 -0400

    Create m4t1_kelly.cpp

---
+++ m4t1_kelly.cpp
@@ -0,0 +1,21 @@
+// CSC 134
+// M4T1 - While
+// Ricardo Kelly
+// March 9, 2025
+
+#include <iostream>
+using namespace std;
+  
+int main()
+{
+
+    // Part 1, just say "hi" five times
+    int count = 1;
+
+    while (count <= 5) {
+        cout << "Hello number " << count << endl;
+        count++;
+    }
+
+    return 0; // done
+}
